a28fdb672c40595edd555b7ced8105be15d2b025
Jeff <writetojeffrey@gmail.com>
2017-09-29 00:14:53 -0700

    Script for copying validation set to separate directory.

---
+++ explore_subset.py
@@ -0,0 +1,40 @@
+from shutil import copyfile
+import os
+
+def makedirs(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+def copy_subset(image_list_file,
+                img_folder,
+                pascal_folder,
+                dest_img_folder,
+                dest_pascal_folder):
+
+    makedirs(dest_img_folder)
+    makedirs(dest_pascal_folder)
+
+    with open(image_list_file, 'r') as f:
+        for line in f:
+            basename_no_ext = line.strip()
+
+            img_filename = os.path.join(img_folder, basename_no_ext + '.jpg')
+            dest_img_filename = os.path.join(dest_img_folder, basename_no_ext + '.jpg')
+
+            copyfile(img_filename, dest_img_filename)
+
+            pascal_filename = os.path.join(pascal_folder, basename_no_ext + '.xml')
+            dest_pascal_filename = os.path.join(dest_pascal_folder, basename_no_ext + '.xml')
+
+            copyfile(pascal_filename, dest_pascal_filename)
+
+
+def copy():
+    copy_subset('/home/eljefec/data/nexet/ImageSets/Main/test.txt',
+                '/home/eljefec/data/nexet/train',
+                '/home/eljefec/data/nexet/train_pascal',
+                '/home/eljefec/data/nexet/val',
+                '/home/eljefec/data/nexet/val_pascal')
+
+if __name__ == '__main__':
+    copy()
