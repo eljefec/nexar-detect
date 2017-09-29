6517518015aa147f9f4fb8b19b14906eccd1c143
Jeff <writetojeffrey@gmail.com>
2017-09-29 04:40:51 -0700

    Support evaluating generated detections.

--- generate_detections.py
+++ generate_detections.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from keras_frcnn_lib.test_frcnn import FRCNNTester
+from challenge2_evaluation.evaluate.eval_challenge import eval_detector_csv
 from nexet_to_pascal_voc import DatasetBuilder
 import os
 import cv2
@@ -9,12 +10,13 @@ HEADER = 'image_filename,x0,y0,x1,y1,label,confidence'
 NUM_ROIS = 32
 
 def detect_folder(model, img_folder, dt_csv):
+    count = 0
     with open(dt_csv, 'w') as f:
         print(HEADER, file=f)
         for img_name in os.listdir(img_folder):
             if not img_name.lower().endswith(('.bmp', '.jpeg', '.jpg', '.png', '.tif', '.tiff')):
                 continue
-            print(img_name)
+            print('{}: {}'.format(count, img_name))
             filepath = os.path.join(img_folder, img_name)
 
             img = cv2.imread(filepath)
@@ -23,10 +25,25 @@ def detect_folder(model, img_folder, dt_csv):
             for box in pred_boxes:
                 print('{},{},{},{},{},{},{}'.format(img_name, box.x1, box.y1, box.x2, box.y2, box.class_name, box.prob), file=f)
 
+            count += 1
+
 def detect_val():
     site.addsitedir('./keras_frcnn_lib')
-    frcnn = FRCNNTester('config.pickle', NUM_ROIS)
-    detect_folder(frcnn, '/home/eljefec/data/nexet/val', '/home/eljefec/data/nexet/dt/frcnn_dt.csv')
+    for bbox_threshold in [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]:
+        frcnn = FRCNNTester('config.pickle', NUM_ROIS)
+        dt_csv = '/home/eljefec/data/nexet/dt/val_dt_frcnn_bb{}.csv'.format(bbox_threshold)
+        detect_folder(frcnn, '/home/eljefec/data/nexet/val', dt_csv)
+        yield dt_csv
+
+def try_detect_val():
+    with open('exp_frcnn_bbox.txt', 'w') as write_f:
+        gt_csv = '/home/eljefec/data/nexet/val_gt.csv'
+        for dt_csv in detect_val():
+            iou_threshold = 0.75
+            ap = eval_detector_csv(gt_csv, dt_csv, iou_threshold)
+            report = '{}: {}'.format(dt_csv, ap)
+            print(report, file=write_f)
+            print(report)
 
 def generate_groundtruth(pascal_csv, gt_csv):
     dataset = DatasetBuilder()
@@ -44,4 +61,5 @@ def groundtruth_val():
 
 if __name__ == '__main__':
     # detect_val()
-    groundtruth_val()
+    # groundtruth_val()
+    try_detect_val()
