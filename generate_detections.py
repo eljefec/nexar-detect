9582d86e282356fcea96a73882c2b267ca124032
Jeff <writetojeffrey@gmail.com>
2017-09-30 02:41:48 -0700

    Generate and evaluate detections by RFCN on validation set.

--- generate_detections.py
+++ generate_detections.py
@@ -27,7 +27,7 @@ def detect_folder(model, img_folder, dt_csv):
 
             count += 1
 
-def detect_val():
+def detect_val_frcnn():
     site.addsitedir('./keras_frcnn_lib')
     for bbox_threshold in [0.7, 0.6, 0.4, 0.2]:
         frcnn = FRCNNTester('config.pickle', NUM_ROIS, bbox_threshold)
@@ -35,14 +35,25 @@ def detect_val():
         detect_folder(frcnn, '/home/eljefec/data/nexet/val', dt_csv)
         yield dt_csv
 
-def try_detect_val():
+def detect_val_rfcn():
+    site.addsitedir('./RFCN_tensorflow')
+    from RFCN_tensorflow.test import RFCNTester
+    for bbox_threshold in [0.6, 0.7, 0.8, 0.9]:
+        rfcn = RFCNTester('RFCN_tensorflow/save/save', None, bbox_threshold)
+        dt_csv = '/home/eljefec/data/nexet/dt/val_dt_rfcn_bb{}.csv'.format(bbox_threshold)
+        detect_folder(rfcn, '/home/eljefec/data/nexet/val', dt_csv)
+        yield dt_csv
+
+def try_detect_val(detect_val_func, report_filename):
     iou_threshold = 0.75
-    with open('exp_frcnn_bbox.txt', 'w') as write_f:
+    with open(report_filename, 'w') as write_f:
         gt_csv = '/home/eljefec/data/nexet/val_gt.csv'
-        for dt_csv in detect_val():
+        for dt_csv in detect_val_func():
             ap = eval_detector_csv(gt_csv, dt_csv, iou_threshold)
             report = '{}: {}'.format(dt_csv, ap)
             print(report, file=write_f)
+            write_f.flush()
+            os.fsync(write_f.fileno())
             print(report)
 
 def generate_groundtruth(pascal_csv, gt_csv):
@@ -62,4 +73,5 @@ def groundtruth_val():
 if __name__ == '__main__':
     # detect_val()
     # groundtruth_val()
-    try_detect_val()
+    # try_detect_val(detect_val_frcnn, 'exp_frcnn_bbox.txt')
+    try_detect_val(detect_val_rfcn, 'exp_rfcn_bbox.txt')
